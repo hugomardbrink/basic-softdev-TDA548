0f293a1f0a11653c6b8bea95f54db8a271ff6150
Hugo Mårdbrink <59877080+hugomardbrink@users.noreply.github.com>
2020-10-28 22:14:26 +0100

    Update and rename staticObject.java to StaticObject.java

--- Pong/model/StaticObject.java
+++ Pong/model/StaticObject.java
@@ -1,11 +1,11 @@
 package pong.model;
 
 
-public class staticObject extends hitboxObject {
+public class staticObject extends HitboxObject {
     private final double xPos;
     private final double yPos;
 
-    public staticObject(double xPos, double yPos, double width, double height) {
+    public StaticObject(double xPos, double yPos, double width, double height) {
         super(width, height);
         this.xPos = xPos;
         this.yPos = yPos;
